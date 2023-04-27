713bbe733ee629cb340c787b8f0d9488568d0184
Henrique <hrlima7@gmail.com>
2023-04-27 20:20:13 -0300

    BACKEND JWT BCRYPT

--- src/app.js
+++ src/app.js
@@ -2,18 +2,13 @@ const express = require("express");
 require("express-async-errors");
 const auth = require("./controllers/loginController");
 const doLogin = require("./controllers/loginController");
-
-
-
 const cors = require("cors");
 const helmet = require("helmet");
-
 
 require("express-async-errors");
 
 
 const app = express();
-
 app.use(express.json());
 
 app.use(cors());
