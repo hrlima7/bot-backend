69c56d6360171f75f5f8082ca5665ebaf88fca72
Henrique <hrlima7@gmail.com>
2023-04-27 21:41:40 -0300

    validacao JWT no backend

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 const express = require("express");
+
 require("express-async-errors");
-const auth = require("./controllers/loginController");
+const loginMiddleware = require("./middleware/loginMIddleware")
+const settingsController = require("./controllers/settingsController");
 const doLogin = require("./controllers/loginController");
 const cors = require("cors");
 const helmet = require("helmet");
-
 require("express-async-errors");
 
 
+
 const app = express();
+
 app.use(express.json());
 
 app.use(cors());
 
 app.use(helmet());
+
 app.post('/login', doLogin);
 
+app.get('/settings',loginMiddleware, settingsController.getSettings);
+
+app.post('/logout', doLogout);
 
 app.use((error,req,res) =>{
 console.error(error);
